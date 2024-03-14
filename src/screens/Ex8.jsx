8188aa0f9dc48861c2226124a58ef13b59831dbf
HarshuNK <harshithank58@gmail.com>
2024-03-14 14:01:25 +0530

    modified

--- src/screens/Ex8.jsx
+++ src/screens/Ex8.jsx
@@ -1,6 +1,17 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
+import Marks from '../Pages/Marks'
+import Info from '../Pages/Info'
 
 function Ex8() {
+
+  const [age,setAge] = useState(10)
+
+  const incAge = (val) => {
+    setAge(age + val)
+  }
+  const rajsAge = useMemo(() => ({
+    result: `Raj's age is ${age} years`
+  }),[age])
   return (
     <div className="conatiner">
         <div className="row">
@@ -8,6 +19,9 @@ function Ex8() {
                 <h3 className="display-3 text-success"> useMemo Hooks</h3>
             </div>
         </div>
+        <Marks age={age} add={incAge}/>
+        <Info txt={rajsAge}/>
+
     </div>
   )
 }
